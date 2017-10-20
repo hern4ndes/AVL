104d598b532cab8871b3e8c2211497d9186c632f
Diego Winter <diego.winter@outlook.com>
2017-10-20 18:24:40 -0200

    Localização
    Implementada a localização.
    Obs.: Não mostra mensagem se um nó não for encontrado.

--- AVL.py
+++ AVL.py
@@ -10,27 +10,27 @@ class Node:
         self.direita = direita
 
     def FB(self):
-        #Usa as profundidades para calcular os fatores de balanceamento
-        profE = 0
+        #Usa as alturas para calcular os fatores de balanceamento
+        alturaE = 0
         if self.esquerda:
-            profE = self.esquerda.profundidade()
-        profD = 0
+            alturaE = self.esquerda.altura()
+        alturaD = 0
         if self.direita:
-            profD = self.direita.profundidade()
-        return profD - profE
+            alturaD = self.direita.altura()
+        return alturaD - alturaE
 
-    def profundidade(self):
-        #Calcula a profundidade dos lados da arvore (esquerdo ou direito)
+    def altura(self):
+        #Calcula a altura dos lados da arvore (esquerdo ou direito)
         #Basicamente entra recursivamente arvore ate achar o fim. Sao duas variaveis sendo incrementadas.
-        #Como a profundidade pode envolver lado oposto(no caso do esquerdo, se nao haver mais esquerdo, pode se procurar filho direito pra continuar calculando)
-        #E no final eh retornado o que incrementou mais(que e o que realmente vale)
-        profE = 0
+        #Como o calculo da altura pode envolver lado oposto(no caso do esquerdo, se nao haver mais esquerdo, pode se procurar filho direito pra continuar calculando)
+        #E no final eh retornado 1+ o que incrementou mais(que e o que realmente vale)
+        alturaE = 0
         if self.esquerda:
-            profE = self.esquerda.profundidade()
-        profD = 0
+            alturaE = self.esquerda.altura()
+        alturaD = 0
         if self.direita:
-            profD = self.direita.profundidade()
-        return 1 + max(profE, profD) #Retorna 1 + a mais incrementada
+            alturaD = self.direita.altura()
+        return 1 + max(alturaE, alturaD) #Retorna 1 + a mais incrementada
 
     def rot_E(self):
         #Realiza a operacao de rotacao simples a esquerda
@@ -84,6 +84,24 @@ class Node:
                 self.direita.insere(chave)
         self.executaBalanco() #Apos inserir executa o balanceamento da arvore
 
+    def localizar(self, chave, pai="-"):
+        if chave < self.chave: #se a chave procurada foi menor que a chave atual, vai pra esquerda, recursivamente, salvando a chave atual no caso de este ser o pai do procurado
+            pai = str(self.chave) #guarda o pai pro caso de ser o proximo no o desejado
+            if self.direita is not None:
+                self.esquerda.localizar(chave, pai)
+        elif chave > self.chave: #se a chave procurada foi maior que a chave atual, vai pra direita, recursivamente, salvando a chave atual no caso de este ser o pai do procurado
+            pai = str(self.chave) #guarda o pai pro caso de ser o proximo no o desejado
+            if self.direita is not None:
+                self.direita.localizar(chave, pai)
+        elif chave == self.chave: #Se chegar num caso de chave igual, eh porque encontramos. Assim printamos as informacoes
+            filhoesq = "-"
+            filhodir = "-"
+            if self.esquerda is not None:
+                filhoesq = str(self.esquerda.chave)
+            if self.direita is not None:
+                filhodir = str(self.direita.chave)
+            print '\nNo localizado:\n No: ' + str(self.chave) + '\n Pai: ' + str(pai) + '\n Filhos:\n  Esquerda: ' + filhoesq + '\n  Direita: ' + filhodir + '\n FB: ' + str(self.FB())
+
     def imprimeArvore(self, indent = 0, count = 0, pai = 0):
         #Printa a arvore na ordem raiz -> filho dir - filho esq. Mostra o pai de cada no e seu respectivo fator de balanceamento
         count = count + 1
@@ -96,10 +114,11 @@ class Node:
             pai = 0
 
         #navega recursivamente na arvore levando como paramentros instrucoes para a printagem
-        if self.direita:
-            self.direita.imprimeArvore(indent + 2, count + 2, pai + self.chave)
         if self.esquerda:
             self.esquerda.imprimeArvore(indent + 2, count + 2, pai + self.chave)
+        if self.direita:
+            self.direita.imprimeArvore(indent + 2, count + 2, pai + self.chave)
+
 
 incl_raiz = False #Var. global que informa se ha uma raiz inserida, ou seja, se o __init_ da classe ja foi realizado
 
@@ -107,9 +126,9 @@ def menu():
     #Exibe um menu simples para auxiliar o usuario
     validacao = True #true neste caso assume a invalidez da resposta
     while validacao:
-        print("1. INPUT.txt\n2. Inserir manualmente\n3. Exibir arvore (identacao)\n0. Sair\n")
+        print("\n1. INPUT.txt\n2. Inserir manualmente\n3. Exibir arvore (identacao)\n4. Localizar\n0. Sair\n")
         resp = input('Resposta: ')
-        if resp<0 or resp >3:
+        if resp<0 or resp >4:
             validacao = True
         else:
             validacao = False
@@ -136,6 +155,8 @@ def main():
         elif resp is 3: #chama a funcao imprimeArvore da classe Node para imprimir a arvore
             arvore.imprimeArvore()
             print ""
+        elif resp is 4:
+            arvore.localizar(input('Chave do no para localizar: '))
         elif resp is 0: #da ordem de saida do main e consequentemente do programa
             return 0
 
@@ -143,16 +164,10 @@ def read():
     #Le os elementos do arquivo e os coloca em um vetor de inteiros
     arquivo = open("INPUT.TXT","r") #abre arquivo com nos
     nodes = arquivo.read().split(';') #vetor nodes recebe os elementos no formato string
-    if nodes[-1] is "": #Se for usado ";" o split vai ler um elemento vazio. Entao chama a funcao abaixo para remover esse elemento vazio
-        nodes.pop() #removendo o ultimo elemento que e um elemento vazio (do editor de texto)
-    else: #Caso foi usado "." no fim, ele transforma a "chave." (chave com ponto) em um float(por causa do ponto) e depois em int, para ser transformado em chaves para nos na lista nodes
-        ultimo = float(nodes[-1]) #
-        nodes[-1] = int(ultimo)
+    ultimo = float(nodes[-1])
+    nodes[-1] = int(ultimo)
     nodes = [int(x) for x in nodes] #transformacao dos elementos do vetor de string em vetor de inteiros
     return nodes #retornando um vetor de inteiros
-
-import os
-limpaTela = lambda: os.system('cls') # para limpar a tela
 
 if __name__ == '__main__':
     main()
