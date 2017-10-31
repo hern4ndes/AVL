a48b78d6d384d61fc17dc9701510187917315643
Diego Winter <diego.winter@outlook.com>
2017-10-31 00:20:55 -0200

    + Comentários

--- AVL.py
+++ AVL.py
@@ -110,24 +110,32 @@ class Node:
     def remover(self, chave):
         global incl_raiz
         #avalia o caso especial de remocao e depois manda pra remocao
+        #o caso especial é quando temos apenas dois nos, e a raiz deve ser excluida
         if chave == self.chave:
             if self.esquerda is None and self.direita is not None:
+                #se tiver apenas a raiz e um no na direita
                 self.chave = self.direita.chave
                 self.filhos = self.direita.filhos
                 self.direita = self.direita.direita
             elif self.direita is None and self.esquerda is not None:
+                #se tiver apenas a raiz e um no na esquerda
                 self.chave = self.esquerda.chave
                 self.filhos = self.esquerda.filhos
                 self.esquerda = self.esquerda.esquerda
             elif self.direita is None and self.esquerda is None:
-                self.__init__('Arvore vazia. Insira nos.')
+                #se tiver restando apenas a raiz, ele troca a raiz por uma mensagem de arvore vazia
+                self.__init__('Vazio')
                 incl_raiz = False
             else:
+                #se nao for nenhum dos casos acima, faz a remocao com os outros casos no outro metodo
+                #nesse caso, se o no a ser removido for uma raiz mas nao dos casos espciais acima
                 self.remocao(chave)
         else:
+            #qualquer outro no ou caso de remocao
             self.remocao(chave)
 
     def remocao(self, chave):
+        #remocao de no folha ou no com apenas um filho, recursivamente. usa os retornos a cada recursao pra percorrer e substituir o no apagado
         if chave < self.chave:
             self.esquerda = self.esquerda.remocao(chave)
         elif chave > self.chave:
@@ -137,41 +145,55 @@ class Node:
                 return self.esquerda
             if self.esquerda is None:
                 return self.direita
+            #remocao de no com dois filhos. substitui o no removido pelo menor no da subarvore da direita. tem dois metodos auxiliarres
+            #o metodo menor busca o menor no da subarvore da direita. o metodo deletamenor deleta esse no, pois ele ja foi substiuir o no removido
             aux = self.direita.menor()
             self.chave = aux.chave
             self.direita = self.direita.deletamenor()
         return self
 
     def menor(self):
+        #busca o menor no da subarvore direita do no removido
         if self.esquerda is None:
             return self
         else:
             return self.esquerda.menor()
 
     def deletamenor(self):
+        #exclui o menor no da subarvore da direita, pois esse no foi substituir o no removido
         if self.esquerda is None:
             return self.direita
         self.esquerda = self.esquerda.deletamenor()
         return self
 
     def rebalanceamento(self):
+        #Apos uma remocao, este metodo busca nos com FBs maiores que um ou menores que -1 para realizar o balanceamento. Utiliza logica semelhante a da funcao
+        #de rebalanceamento ao inserir um no
         if self.esquerda is not None:
             self.esquerda.rebalanceamento()
-        bal = self.FB()
+            
+        bal = self.FB()#bal recebe o FB do no acessado atualmente
         if bal > 1:
+            #FBs>1 fazem rotacoes a esquerda
             if self.direita.direita is not None:
+                #se o no da direita da direita existir, entao sera uma rotacao simples a esquerda
                 print 'Rebalanceamento: Rotacao a esquerda em ' + str(self.chave) + ' que tinha o FB ' + str(self.FB())
                 self.rot_E()
             else:
+                #caso nao, sera dupla a esquerda
                 print 'Rebalanceamento: Rotacao dupla a esquerda em ' + str(self.chave) + ' que tinha o FB ' + str(self.FB())
                 self.rot_dup_E()
         elif bal < -1:
+            #FBs<-1 fazem rotacoes a direita
             if self.esquerda.esquerda is not None:
+                #se o no da direita da direita existir, entao sera uma rotacao simples a direita
                 print 'Rebalanceamento: Rotacao a direita em ' + str(self.chave) + ' que tinha o FB ' + str(self.FB())
                 self.rot_D()
             else:
+                #caso nao, sera dupla a direita
                 print '\nRebalanceamento: Rotacao dupla a direita em \n' + str(self.chave) + ' que tinha o FB ' + str(self.FB())
                 self.rot_dup_D()
+                
         if self.direita is not None:
             self.direita.rebalanceamento()
 
@@ -185,7 +207,6 @@ class Node:
         else:
             print " " * indent + str(self.chave) +'  (pai: ' + str(pai) + ' FB: '+ str(self.FB()) + ')'
             pai = 0
-
         #navega recursivamente na arvore levando como paramentros instrucoes para a printagem
         if self.esquerda:
             self.esquerda.imprimeArvore(indent + 2, count + 2, pai + self.chave)
@@ -194,8 +215,11 @@ class Node:
 
     def calculaNiveis(self, rootOk = 0, nivelRoot = 0):
         #calcula os niveis dos nos pra ajudar no calculo da posicao
+        #coloca dentro de cada no o seu nivel (ou nivel aproximado em alguns casos), apenas para auxiliar na orientacao da apresentacao grafica da arvore
+        #note que os niveis comecam com 1 e nao representam o conceito de niveis. Isso e apenas o nome do metodo, que apenas auxilia a interface grafica.
         rootOk = rootOk + 1
         if rootOk == 1:
+            #a altura toda da arvore e o nivel dela
             nivelRoot = self.altura()
         else:
             self.nivel = nivelRoot - self.altura() + 1
@@ -207,28 +231,36 @@ class Node:
     def calcposicao(self,  rootOk = 0, posPai = [0,0], lado = 0):
         nodes = []
         #Calcula as posicoes para orientar o desenho na interface grafica
+        #coloca em cada no sua posicao (ou posicao aproximada) a ser desenhada na interface grafica da arvore, dependendo de seu parentesco entre outros
         rootOk = rootOk + 1
         node = ()
         if rootOk == 1:
+            #da uma posicao centralizada para a raiz
             self.posicao = (1000,  100)
             node = (self.chave,self.posicao)
             nodes.append(node)
         else:
+            #calculo para o restante dos nos
             if lado == 1:
+                #lado 1 = esquerda
                 aux = 2 ** self.nivel
                 aux = aux/2
                 deslocamento = 1000/aux
+                #calcula um deslocamento adequado para o nivel de cada no
                 self.posicao = [posPai[0] - deslocamento, posPai[1]+100]
-                print self.posicao
                 node = (self.chave,self.posicao,posPai)
                 nodes.append(node)
+                #coloca informacoes no vetor nodes para poder levar para a funcao que desenha na tela a arvore
             else:
+                #lado 2 = direita
                 aux = 2 ** self.nivel
                 aux = aux/2
                 deslocamento = 1000/aux
+                #calcula um deslocamento adequado para o nivel de cada no
                 self.posicao = [posPai[0] + deslocamento, posPai[1]+100 ]
                 node = (self.chave,self.posicao,posPai)
                 nodes.append(node)
+                #coloca informacoes no vetor nodes para poder levar para a funcao que desenha na tela a arvore
 
         if self.esquerda:
             self.esquerda.calcposicao(rootOk + 2, posPai = [self.posicao[0], self.posicao[1]], lado = 1)
@@ -236,4 +268,4 @@ class Node:
             self.direita.calcposicao (rootOk + 2, posPai = [self.posicao[0], self.posicao[1]], lado = 2)
 
         return nodes
-
+        #retorna o vetor de nos.
